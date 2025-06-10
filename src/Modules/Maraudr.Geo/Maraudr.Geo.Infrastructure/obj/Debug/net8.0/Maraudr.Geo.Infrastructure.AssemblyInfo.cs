6135cfb013752970dbf5641946f476ed02a7101f
xororist <hcachon@myges.fr>
2025-06-10 22:17:20 +0200

    fix jwt to retrieve user id

--- src/Modules/Maraudr.Geo/Maraudr.Geo.Infrastructure/obj/Debug/net8.0/Maraudr.Geo.Infrastructure.AssemblyInfo.cs
+++ src/Modules/Maraudr.Geo/Maraudr.Geo.Infrastructure/obj/Debug/net8.0/Maraudr.Geo.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maraudr.Geo.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f19f4f051df07b453a7c8336ede9bc73465d96e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b47c78f3f8303891e165f053a12b320cd10596")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maraudr.Geo.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maraudr.Geo.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
