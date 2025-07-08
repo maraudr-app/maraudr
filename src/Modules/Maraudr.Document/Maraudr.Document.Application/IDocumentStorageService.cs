7b44a558f84b33650d997dc14316022ede6dcf99
xororist <hcachon@myges.fr>
2025-07-08 19:57:02 +0200

    pre signed url for documents

--- src/Modules/Maraudr.Document/Maraudr.Document.Application/IDocumentStorageService.cs
+++ src/Modules/Maraudr.Document/Maraudr.Document.Application/IDocumentStorageService.cs
@@ -6,4 +6,6 @@ public interface IDocumentStorageService
 {
     Task<(string Url, string Key)> UploadAsync(IFormFile file, Guid associationId);
     Task DeleteAsync(string key);
+    Task<string> GeneratePresignedUrlAsync(string key, TimeSpan validFor);
+
 }
