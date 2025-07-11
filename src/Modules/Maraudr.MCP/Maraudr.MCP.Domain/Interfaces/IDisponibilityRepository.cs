3eb18bb02e130c7ac9f80acfb75e31a9e2b6961f
MAKHLOUFI Khalil <kmakhlou@bouyguestelecom.fr>
2025-07-11 16:25:29 +0200

    feat(mcp): disponibilities tools added

---
+++ src/Modules/Maraudr.MCP/Maraudr.MCP.Domain/Interfaces/IDisponibilityRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Maraudr.MCP.Domain.Interfaces;
+
+public record DisponibilityDto(
+ Guid Id,
+ Guid UserId,
+ DateTime Start,
+ DateTime End,
+ Guid AssociationId); // Champ obligatoire pour l'association);
+public interface IDisponibilityRepository
+{
+ public Task<IEnumerable<DisponibilityDto>> GetMyDisponibilitiesInAssociation(Guid associationId,string jwt);
+ 
+ public Task<IEnumerable<DisponibilityDto>> GetAllDisponibilitiesInAssociation(Guid associationId,string jwt);
+
+}
---
+++ src/Modules/Maraudr.MCP/Maraudr.MCP.Infrastructure/Repositories/DisponbilityRepository.cs
@@ -0,0 +1,73 @@
+﻿using System.Net.Http.Json;
+using Maraudr.MCP.Domain.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace Maraudr.MCP.Infrastructure.Repositories;
+
+public class DisponbilityRepository(HttpClient httpClient, IOptions<ApiSettings> options):IDisponibilityRepository
+{
+    private static readonly string LogFilePath = Path.Combine("/tmp", "association-repository.log");    
+    private void LogToFile(string message)
+    {
+        try
+        {
+            var logMessage = $"{DateTime.UtcNow:O} - {message}{Environment.NewLine}";
+            File.AppendAllText(LogFilePath, logMessage);
+            
+        }
+        catch
+        {
+            // Ignorer les erreurs de journalisation
+        }
+    }
+    public async Task<IEnumerable<DisponibilityDto>> GetMyDisponibilitiesInAssociation(Guid associationId,string jwt)
+    {
+        var associationUrl = $"{options.Value.UserApiUrl}/{associationId}";
+        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",jwt);
+        var response = await httpClient.GetAsync(associationUrl);
+        try
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                LogToFile("Error while getting user disponibility in association");
+                LogToFile($"Response status :"+response.StatusCode);
+                return null;
+            }
+            LogToFile("Success");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<DisponibilityDto?>>();
+          
+        }
+        catch (Exception ex)
+        {
+            LogToFile("Error while getting all events from PlanningRepository");
+            LogToFile($"Error:{ex.StackTrace}");
+            return null;
+        }    
+    }
+    
+    
+    public async Task<IEnumerable<DisponibilityDto>> GetAllDisponibilitiesInAssociation(Guid associationId,string jwt)
+    {
+        var associationUrl = $"{options.Value.UserApiUrl}all/{associationId}";
+        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",jwt);
+        var response = await httpClient.GetAsync(associationUrl);
+        try
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                LogToFile("Error while getting user disponibility in association");
+                LogToFile($"Response status :"+response.StatusCode);
+                return null;
+            }
+            LogToFile("Success");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<DisponibilityDto?>>();
+          
+        }
+        catch (Exception ex)
+        {
+            LogToFile("Error while getting all events from PlanningRepository");
+            LogToFile($"Error:{ex.StackTrace}");
+            return null;
+        }    
+    }
+}
