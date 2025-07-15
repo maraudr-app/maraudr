034329b7ebe15a700e732dd3a953a57ce90cb4bb
Khali <khalilmakhloufi26@gmail.com>
2025-07-15 23:38:21 +0200

    fix(email): fixed email problem

--- src/Modules/Maraudr.User/Maraudr.User.Application/UseCases/Users/User/CreateUser/CreateUserHandler.cs
+++ src/Modules/Maraudr.User/Maraudr.User.Application/UseCases/Users/User/CreateUser/CreateUserHandler.cs
@@ -12,12 +12,15 @@ public class CreateUserHandler(IUserRepository repository,IPasswordManager passw
 
     public async Task<Guid> HandleAsync(CreateUserDto createUserDto)
     {
-        if(await repository.GetByEmailAsync(createUserDto.Email) != null)
-        {
-            throw new InvalidOperationException($"L'email {createUserDto.Email} est déjà utilisé.");
-        }
+        var currentUser = await repository.GetByEmailAsync(createUserDto.Email);
+       
         if (createUserDto.IsManager)
         {
+            if( currentUser != null)
+            {
+                throw new InvalidOperationException($"L'email {createUserDto.Email} est déjà utilisé.");
+            }
+            
             var manager = CreationCommandToManager.MapCreationCommandToManager(createUserDto,passwordManager);
             await repository.AddAsync(manager);
             await mailSenderRepository.SendWelcomeEmailTo(manager.ContactInfo.Email, manager.Firstname);
@@ -29,21 +32,28 @@ public class CreateUserHandler(IUserRepository repository,IPasswordManager passw
             {
                 throw new InvalidOperationException($"Le manager avec le token {createUserDto.ManagerToken} n'existe pas.");
             }
-            
 
-            var user = CreationCommandToUser.MapCreationCommandToUser(createUserDto,manager,passwordManager);
-            if (user == null)
+            if (currentUser == null)
             {
-                throw new InvalidOperationException("Erreur lors de la création de l'utilisateur.");
+                currentUser = CreationCommandToUser.MapCreationCommandToUser(createUserDto,manager,passwordManager);
+                if (currentUser == null)
+                {
+                    throw new InvalidOperationException("Erreur lors de la création de l'utilisateur.");
+                }
             }
             
             var cManager = (Maraudr.User.Domain.Entities.Users.Manager)manager;
-            cManager.AddMemberToTeam(user);
-            await repository.AddAsync(user);
+            if (cManager.Team.Contains(currentUser))
+            {
+                throw new InvalidOperationException($"Vous êtes déjà membre de l'équipe ");
+ 
+            }
+            cManager.AddMemberToTeam(currentUser);
+            await repository.AddAsync(currentUser);
             await repository.UpdateAsync(cManager);
             await repository.InvalidateExistingInvitationsAsync(createUserDto.Email);
-            await mailSenderRepository.SendWelcomeEmailTo(user.ContactInfo.Email, user.Firstname);
-            return user.Id;
+            await mailSenderRepository.SendWelcomeEmailTo(currentUser.ContactInfo.Email, currentUser.Firstname);
+            return currentUser.Id;
         }
         
         
