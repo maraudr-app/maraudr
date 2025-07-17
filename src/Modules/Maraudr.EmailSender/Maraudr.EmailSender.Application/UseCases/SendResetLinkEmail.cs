4b717968fc8c2006f891e65158c657ae55c0f9db
xororist <hcachon@myges.fr>
2025-07-17 15:22:29 +0200

    changing url for reset password use cases

--- src/Modules/Maraudr.EmailSender/Maraudr.EmailSender.Application/UseCases/SendResetLinkEmail.cs
+++ src/Modules/Maraudr.EmailSender/Maraudr.EmailSender.Application/UseCases/SendResetLinkEmail.cs
@@ -18,7 +18,7 @@ public class SendResetLinkEmailHandler(IMailService mailService) : ISendResetLin
     {
         try
         {
-            var resetLink = $"https://www.maraudr.eu/reset-password?token={Uri.EscapeDataString(query.Token)}";
+            var resetLink = $"https://maraudr-front-737l.onrender.com/reset-password?token={Uri.EscapeDataString(query.Token)}";
             await mailService.SendEmailAsync(new MailRequest
             {
                 ToEmail = query.ToEmail,
